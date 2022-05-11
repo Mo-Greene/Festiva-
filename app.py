350c625ae0c0beaa6f2d630a257ef8b5aabf6597
kim_geunyeong <rootzero17@gmail.com>
2022-05-11 17:22:40 +0900

    Add profile img to mypage.html and Fix app.py

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
-from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+
+
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.lovi7.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
 
 @app.route('/mypage')
 def mypage():
