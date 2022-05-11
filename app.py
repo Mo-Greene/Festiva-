017cc952308b6b4c1bad0f6f53400e71e59c8cb4
horang-e <rmsid1000@gmail.com>
2022-05-11 18:08:36 +0900

    html이름 수정

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
  r = requests.get("http://api.data.go.kr/openapi/tn_pubr_public_cltur_fstvl_api?serviceKey=2%2FK1CdSKKycm%2FIyr1z09L2cFGNZIOO0uBgTNREIj3m8CbuZg5jcGqGzQV%2FhKIbphrEEOOeoxzwyj4vgco6M1bg%3D%3D&pageNo=0&numOfRows=100&type=json")
  response = r.json()
  items = response['response']['body']['items']
- return render_template('index.html', items=items)
+ return render_template('festiv.html', items=items)
 
 
 if __name__ == '__main__':
