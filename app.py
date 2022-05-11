8c438be77c0d75954748d3573951d7b6283d4cd2
horang-e <rmsid1000@gmail.com>
2022-05-11 16:14:20 +0900

    og태그는 작동하지 않음

--- app.py
+++ app.py
@@ -1,50 +1,22 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 app = Flask(__name__)
+
+import requests
+from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.diiam.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
-#mflick > div > div > div > div:nth-child(1) > div:nth-child(1) > div.data_area > div > div.title > div > strong > a
 
-@app.route('/')
+
+@app.route('/festival')
 def home():
- return render_template('index.html')
+ r = requests.get("http://api.data.go.kr/openapi/tn_pubr_public_cltur_fstvl_api?serviceKey=2%2FK1CdSKKycm%2FIyr1z09L2cFGNZIOO0uBgTNREIj3m8CbuZg5jcGqGzQV%2FhKIbphrEEOOeoxzwyj4vgco6M1bg%3D%3D&pageNo=0&numOfRows=100&type=json")
+ response = r.json()
+ items = response['response']['body']['items']
+ return render_template('index.html', items=items)
 
-@app.route("/bucket", methods=["POST"])
-def bucket_post():
- bucket_receive = request.form['bucket_give']
-
- bucket_list = list(db.bucket.find({}, {'_id': False}))
- count = len(bucket_list) + 1
-
- doc={
-     'num':count,
-     'bucket':bucket_receive,
-     'done':0
- }
-
- db.bucket.insert_one(doc)
- return jsonify({'msg': '등록 완료!'})
-
-@app.route("/bucket/done", methods=["POST"])
-def bucket_done():
- num_receive = request.form['num_give']
- db.bucket.update_one({'num':int(num_receive)}, {'$set': {'done': 1}})
- return jsonify({'msg': '버킷리스트 달성!'})
-
-@app.route("/bucket/cancel", methods=["POST"])
-def bucket_cancel():
- num_receive = request.form['num_give']
- db.bucket.update_one({'num':int(num_receive)}, {'$set': {'done': 0}})
- return jsonify({'msg': '다시도전!'})
-
-
-@app.route("/bucket", methods=["GET"])
-def bucket_get():
- bucket_list = list(db.bucket.find({}, {'_id': False}))
-
- return jsonify({'buckets': bucket_list})
 
 if __name__ == '__main__':
  app.run('0.0.0.0', port=5000, debug=True)
